fb167d8778d8ea8ad18d6e442ce92314905e44c5
valarmathi4 <mathicsd2005@gmail.com>
2026-07-28 19:46:41 +0530

    Time: 0 ms (100%), Space: 42 MB (55.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,23 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[]=new int[n+1];
+       /* int dp[]=new int[n+1];
         int max=0;
         dp[0]=1;
-        dp[1]=2;
+        dp[1]=2;*/
         if(n<=2){
             return n;
-        }
+        }/*
         for(int i=2;i<n;i++){
             dp[i]=dp[i-1]+dp[i-2];
             max=Math.max(max,dp[i]);
         }
-        return max;
+        return max;*/
+        int a=1,b=2,c=0;
+        for(int i=2;i<n;i++){
+c=a+b;
+a=b;
+b=c;
+        }
+        return c;
     }
 }
